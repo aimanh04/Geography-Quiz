94e23d2821ab762e2a571346c530c4ff86ff332d
aimanh04 h <aimanhawaemanoh04@gmail.com>
2024-08-25 15:18:47 +0000

    Add responsiveness to your selected answer with function selectAnswer so user can see which answer was correct and wrong and then display block nextButton when answer is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,7 +143,7 @@ function showQuestion() {
 }
 
 function resetState() {
-    nextButton.style.display = "none";
+
     while(answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
     }
@@ -157,6 +157,13 @@ function selectAnswer(e) {
     } else{
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
 
 startQuiz();
